172bd4101a753dd16e3424b5fe1a62355d3eee07
markv <mark.veillette@ll.mit.edu>
2020-02-18 16:00:36 -0500

    Now able to generate lightning samples.  Added more to doc string.

--- sevir/utils.py
+++ sevir/utils.py
@@ -13,8 +13,19 @@ os.environ["HDF5_USE_FILE_LOCKING"]='FALSE'
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.utils import GeneratorEnqueuer
 
+# List all avaialbe types
+TYPES    = ['vis','ir069','ir107','vil','lght']
+
 DEFAULT_CATALOG = '/home/gridsan/groups/EarthIntelligence/datasets/SEVIR/CATALOG.csv'
 DEFAULT_DATA_HOME = '/home/gridsan/groups/EarthIntelligence/datasets/SEVIR/data/'
+
+# Nominal Frame time offsets in minutes (used for non-raster types)
+
+# NOTE:  The lightning flashes in each from will represent the 5 minutes leading up the
+# the frame's time EXCEPT for the first frame, which will use the same flashes as the second frame
+#  (This will be corrected in a future version of SEVIR so that all frames are consistent)
+FRAME_TIMES = np.arange(-120.0,120.0,5) * 60 # in seconds
+#FRAME_TIMES = np.arange(-122.5,127.5,5) * 60 # in seconds  # Alternative def?
 
 class SEVIRSequence(Sequence):
     """
@@ -25,10 +36,57 @@ class SEVIRSequence(Sequence):
     catalog  str or pd.DataFrame
         name of SEVIR catalog file to be read in, or an already read in and processed catalog
     x_img_types  list 
-        List of image types to be used as model inputs
+        List of image types to be used as model inputs.  For types, run SEVIRSequence.get_types()
     y_img_types  list or None
        List of image types to be used as model targets (if None, __getitem__ returns only x_img_types )
-       
+    sevir_data_home  str
+       Directory path to SEVIR data
+    catalog  str
+       Name of SEVIR catalog CSV file.  
+    batch_size  int
+       batch size to generate.  
+    start_date   datetime
+       Start time of SEVIR samples to generate   
+    end_date    datetime
+       End time of SEVIR samples to generate
+    datetime_filter   function
+       Mask function applied to time_utc column of catalog (return true to keep the row). 
+       Pass function of the form   lambda t : COND(t)
+       Example:  lambda t: np.logical_and(t.dt.hour>=13,t.dt.hour<=21)  # Generate only day-time events
+    catalog_filter  function
+       Mask function applied to entire catalog dataframe (return true to keep row).  
+       Pass function of the form lambda catalog:  COND(catalog)
+       Example:  lambda c:  [s[0]=='S' for s in c.id]   # Generate only the 'S' events
+    unwrap_time   bool
+       If True, single images are returned instead of image sequences
+    shuffle  bool
+       If True, data samples are shuffled before each epoch
+    shuffle_seed   int
+       Seed to use for shuffling
+    
+    Returns
+    -------
+    SEVIRSequence generator
+    
+    Examples
+    --------
+    
+        # Get just Radar image sequences
+        vil_seq = SEVIRSequence(x_img_types=['vil'],batch_size=16)
+        X = vil_seq.__getitem__(1234)  # returns list the same size as x_img_types passed to constructor
+        
+        # Get ir satellite+lightning as X,  radar for Y
+        vil_ir_lght_seq = SEVIRSequence(x_img_types=['ir107','lght'],y_img_types=['vil'],batch_size=4)
+        X,Y = vil_ir_lght_seq.__getitem__(420)  # X,Y are lists same length as x_img_types and y_img_types
+        
+        # Get single images of VIL
+        vil_imgs = SEVIRSequence(x_img_types=['vil'], batch_size=256, unwrap_time=True, shuffle=True)
+        
+        # Filter out some times
+        vis_seq = SEVIRSequence(x_img_types=['vis'],batch_size=32,unwrap_time=True,
+                                start_date=datetime.datetime(2018,1,1),
+                                end_date=datetime.datetime(2019,1,1),
+                                datetime_filter=lambda t: np.logical_and(t.dt.hour>=13,t.dt.hour<=21))
     
     """
     def __init__(self,
@@ -36,9 +94,10 @@ class SEVIRSequence(Sequence):
                  y_img_types=None, 
                  catalog=DEFAULT_CATALOG,
                  batch_size = 3,
-                 datetime_filter=None,
                  start_date=None,
                  end_date=None,
+                 datetime_filter=None,
+                 catalog_filter=None,
                  unwrap_time=False,
                  sevir_data_home=DEFAULT_DATA_HOME,
                  shuffle=False,
@@ -68,6 +127,9 @@ class SEVIRSequence(Sequence):
             self.catalog = self.catalog[self.catalog.time_utc <= self.end_date]
         if self.datetime_filter:
             self.catalog = self.catalog[self.datetime_filter(self.catalog.time_utc)]
+        
+        if self.catalog_filter:
+            self.catalog = self.catalog[self.catalog_filter(self.catalog)]
 
         self._compute_samples()
         self._open_files()
@@ -96,7 +158,7 @@ class SEVIRSequence(Sequence):
             resize(imread(file_name), (200, 200))
                for file_name in batch_x]), np.array(batch_y)    
         """
-        batch = self._samples.iloc[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch = self._get_batch_samples(idx)
         data = {}
         for index, row in batch.iterrows():
             data = self._read_data(row,data)
@@ -105,6 +167,8 @@ class SEVIRSequence(Sequence):
         else:
             return [data[t] for t in self.x_img_types],[data[t] for t in self.y_img_types]
     
+    def _get_batch_samples(self,idx):
+        return self._samples.iloc[idx * self.batch_size:(idx + 1) * self.batch_size]
     
     def _read_data(self,row,data):
         """
@@ -114,16 +178,57 @@ class SEVIRSequence(Sequence):
         for t in imgtyps:
             fname = row[f'{t}_filename']
             idx   = row[f'{t}_index']
-            if self.unwrap_time:
-                tidx = row[f'{t}_time_index']
-                data_i = self._hdf_files[fname][t][idx:idx+1,:,:,tidx]
-                data[t] = np.concatenate( (data[t],data_i),axis=0 ) if (t in data) else data_i # TODO: faster to preallocate
+            t_slice = row[f'{t}_time_index'] if self.unwrap_time else slice(0,None)
+            # Need to bin lght counts into grid
+            if t=='lght':
+                lght_data = self._hdf_files[fname][idx][:]
+                data_i = self._lght_to_grid(lght_data,t_slice)
             else:
-                data_i = self._hdf_files[fname][t][idx:idx+1,:,:,:]
-                data[t] = np.concatenate( (data[t],data_i),axis=0 ) if (t in data) else data_i
+                data_i = self._hdf_files[fname][t][idx:idx+1,:,:,t_slice]
+            data[t] = np.concatenate( (data[t],data_i),axis=0 ) if (t in data) else data_i
+            
         return data
 
 
+    def _lght_to_grid(self,data,t_slice=slice(0,None)):
+        """
+        Converts Nx5 lightning data matrix into a 2D grid of pixel counts
+        """
+        #out_size = (48,48,len(FRAME_TIMES)-1) if isinstance(t_slice,(slice,)) else (48,48)
+        out_size = (48,48,len(FRAME_TIMES)) if isinstance(t_slice,(slice,)) else (48,48)
+        if data.shape[0]==0:
+            return np.zeros((1,)+out_size,dtype=np.float32)
+        
+        # filter out points outside the grid
+        x,y=data[:,3],data[:,4]
+        m=np.logical_and.reduce( [x>=0,x<out_size[0],y>=0,y<out_size[1]] )
+        data=data[m,:]
+        if data.shape[0]==0:
+            return np.zeros((1,)+out_size,dtype=np.float32)
+        
+        # Filter/separate times
+        t=data[:,0]
+        if not isinstance(t_slice,(slice,)):  # select only one time bin
+            if t_slice>0:
+                tm=np.logical_and( (t>=FRAME_TIMES[t_slice-1],t<FRAME_TIMES[t_slice]) )
+            else: # special case:  frame 0 uses lght from frame 1
+                tm=np.logical_and( (t>=FRAME_TIMES[0],t<FRAME_TIMES[1]) )
+            #tm=np.logical_and( (t>=FRAME_TIMES[t_slice],t<FRAME_TIMES[t_slice+1]) )
+      
+            data=data[tm,:]
+            z=np.zeros( data.shape[0], dtype=np.int64 )
+        else: # compute z coodinate based on bin locaiton times
+            z=np.digitize(t,FRAME_TIMES)-1
+            z[z==-1]=0 # special case:  frame 0 uses lght from frame 1
+           
+        x=data[:,3].astype(np.int64)
+        y=data[:,4].astype(np.int64)
+        
+        k=np.ravel_multi_index(np.array([y,x,z]),out_size)
+        n = np.bincount(k,minlength=np.prod(out_size))
+        return np.reshape(n,out_size).astype(np.float32)[np.newaxis,:]
+         
+    
     def _compute_samples(self):
         """
         Computes the list of samples in catalog to be used. This sets
@@ -151,13 +256,15 @@ class SEVIRSequence(Sequence):
         df = df.set_index('img_type')
         for i in imgt:
             s = df.loc[i]
-            if not self.unwrap_time:
-                d.update( {f'{i}_filename':[s.file_name], 
-                           f'{i}_index':[s.file_index]} )
-            else:
+            idx = s.file_index if i!='lght' else s.id 
+            if self.unwrap_time:
                 d.update( {f'{i}_filename':[s.file_name]*N_FRAMES, 
-                           f'{i}_index':[s.file_index]*N_FRAMES,
-                           f'{i}_time_index':range(N_FRAMES)} )      
+                           f'{i}_index':[idx]*N_FRAMES,
+                           f'{i}_time_index':range(N_FRAMES)} )   
+            else:
+                d.update( {f'{i}_filename':[s.file_name], 
+                           f'{i}_index':[idx]} )
+                   
         return pd.DataFrame(d)
 
     def _open_files(self):
@@ -178,4 +285,17 @@ class SEVIRSequence(Sequence):
     def on_epoch_end(self):
         if self.shuffle:
             df.sample(frac=1,random_state=self.shuffle)
-
+    
+    def close(self):
+        """
+        Closes all open file handles
+        """
+        for f in self._hdf_files:
+            self._hdf_files[f].close()
+    
+    @staticmethod
+    def get_types():
+        return TYPES
+    
+    
+ 
